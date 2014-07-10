00e1dc02571503af09e56778b5d083af75ecae1c
Nadrieril <nadrieril@gmail.com>
2014-07-10 19:49:11 +0200

    Fixed bad credentials exception

--- src/BR/BarBundle/Controller/AuthController.php
+++ src/BR/BarBundle/Controller/AuthController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Post;
 use FOS\RestBundle\Controller\Annotations\Get;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class AuthController extends FOSRestController {
 
@@ -18,45 +19,46 @@ class AuthController extends FOSRestController {
 	 * @Get("/{bar}/auth/login")
 	 */
 	public function loginAction(Request $request, $bar) {
-		$valid = false;
+		// $login = $request->request->get('login');
+		// $password = $request->request->get('password');
 		$login = $request->query->get('login');
 		$password = $request->query->get('password');
 
-		if($login != null && $password != null) {
-			$repository = $this->getDoctrine()
-					->getRepository('BRBarBundle:Auth\User');
+		if($login == null || $password == null)
+			throw new UnauthorizedHttpException('Bad credentials');
 
-			$users = $repository->createQueryBuilder('u')
-					->where('u.bar = :bar')
-					->andWhere('u.login = :login')
-					->setParameter('bar', $bar)
-					->setParameter('login', $login)
-					->getQuery()->getResult();
+		$repository = $this->getDoctrine()
+				->getRepository('BRBarBundle:Auth\User');
 
-			$valid = (sizeof($users) == 1);
-		}
+		$users = $repository->createQueryBuilder('u')
+				->where('u.bar = :bar')
+				->andWhere('u.login = :login')
+				->setParameter('bar', $bar)
+				->setParameter('login', $login)
+				->getQuery()->getResult();
 
-		if($valid) {
-			$user = $users[0];
-			$pwdEncoder = $this->get('security.encoder_factory')->getEncoder($user);
-			$valid = $pwdEncoder->isPasswordValid(
+		if(sizeof($users) != 1)
+			throw new UnauthorizedHttpException('Bad credentials');
+
+		$user = $users[0];
+
+		$pwdEncoder = $this->get('security.encoder_factory')->getEncoder($user);
+		$valid = $pwdEncoder->isPasswordValid(
 				$user->getPassword(), // the encoded password
 				$password,            // the submitted password
 				$user->getSalt()
 			);
-		}
+		if(!$valid)
+			throw new UnauthorizedHttpException('Bad credentials');
 
-		if($valid) {
-			$payload = array(
+
+		$payload = array(
 				'exp' => time() + $this->container->getParameter('lexik_jwt_authentication.token_ttl'),
 				'username' => $user->getUsername()
-				);
+			);
 
-			$jwt = $this->get('lexik_jwt_authentication.jwt_encoder')->encode($payload)->getTokenString();
+		$jwt = $this->get('lexik_jwt_authentication.jwt_encoder')->encode($payload)->getTokenString();
 
-			return $this->handleView($this->view(array('token' => $jwt), 200));
-		} else {
-			return new JsonResponse('Bad credentials', 401);
-		}
+		return $this->handleView($this->view(array('token' => $jwt, 'user' => $user), 200));
 	}
 }
