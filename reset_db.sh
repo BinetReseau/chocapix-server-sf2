4343f0cb250ff1e12d4389c6caee31b5c42c4027
Nadrieril <nadrieril@gmail.com>
2014-07-10 10:02:51 +0200

    Added database reset script

---
+++ reset_db.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#app/console doctrine:generate:entities --no-backup BR
+
+app/console doctrine:database:drop --force
+app/console doctrine:database:create
+app/console doctrine:schema:update --force
+app/console doctrine:query:sql "$(cat init.sql)"
