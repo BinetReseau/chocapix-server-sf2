1953203e9cc3bc01c77409962ea8d5a1bea1e17e
Nadrieril <nadrieril@gmail.com>
2014-07-19 18:38:55 +0200

    Removed useless getters ans setters

--- src/BR/BarBundle/Entity/Bar/Bar.php
+++ src/BR/BarBundle/Entity/Bar/Bar.php
@@ -12,49 +12,8 @@ class Bar
     /** @ORM\Column(type="string") */
     private $name;
 
-    /**
-     * Set id
-     *
-     * @param string $id
-     * @return Bar
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
 
-        return $this;
-    }
-
-    /**
-     * Get id
-     *
-     * @return string 
-     */
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
-    }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     * @return Bar
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string 
-     */
-    public function getName()
-    {
-        return $this->name;
     }
 }
--- src/BR/BarBundle/Entity/Stock/StockItem.php
+++ src/BR/BarBundle/Entity/Stock/StockItem.php
@@ -23,6 +23,14 @@ class StockItem
      * @JMS\Exclude
      */
     private $bar;
+    /**
+     * @JMS\SerializedName("bar")
+     * @JMS\VirtualProperty
+     */
+    public function getBarId()
+    {
+        return $this->bar->getId();
+    }
 
 
     /** @ORM\Column(type="string", length=255) */
@@ -51,188 +59,5 @@ class StockItem
         $op = new StockOperation($transaction, $this, $deltaqty);
         $transaction->addOperation($op);
         return $op;
-    }
-
-
-    /**
-     * @JMS\SerializedName("bar")
-     * @JMS\VirtualProperty
-     */
-    public function getBarId()
-    {
-        return $this->bar->getId();
-    }
-
-
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     * @return StockItem
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set qty
-     *
-     * @param string $qty
-     * @return StockItem
-     */
-    public function setQty($qty)
-    {
-        $this->qty = $qty;
-
-        return $this;
-    }
-
-    /**
-     * Get qty
-     *
-     * @return string
-     */
-    public function getQty()
-    {
-        return $this->qty;
-    }
-
-    /**
-     * Set unit
-     *
-     * @param string $unit
-     * @return StockItem
-     */
-    public function setUnit($unit)
-    {
-        $this->unit = $unit;
-
-        return $this;
-    }
-
-    /**
-     * Get unit
-     *
-     * @return string
-     */
-    public function getUnit()
-    {
-        return $this->unit;
-    }
-
-    /**
-     * Set price
-     *
-     * @param string $price
-     * @return StockItem
-     */
-    public function setPrice($price)
-    {
-        $this->price = $price;
-
-        return $this;
-    }
-
-    /**
-     * Get price
-     *
-     * @return string
-     */
-    public function getPrice()
-    {
-        return $this->price;
-    }
-
-    /**
-     * Set tax
-     *
-     * @param string $tax
-     * @return StockItem
-     */
-    public function setTax($tax)
-    {
-        $this->tax = $tax;
-
-        return $this;
-    }
-
-    /**
-     * Get tax
-     *
-     * @return string
-     */
-    public function getTax()
-    {
-        return $this->tax;
-    }
-
-    /**
-     * Set keywords
-     *
-     * @param string $keywords
-     * @return StockItem
-     */
-    public function setKeywords($keywords)
-    {
-        $this->keywords = $keywords;
-
-        return $this;
-    }
-
-    /**
-     * Get keywords
-     *
-     * @return string
-     */
-    public function getKeywords()
-    {
-        return $this->keywords;
-    }
-
-    /**
-     * Set bar
-     *
-     * @param \BR\BarBundle\Entity\Bar\Bar $bar
-     * @return StockItem
-     */
-    public function setBar(\BR\BarBundle\Entity\Bar\Bar $bar = null)
-    {
-        $this->bar = $bar;
-
-        return $this;
-    }
-
-    /**
-     * Get bar
-     *
-     * @return \BR\BarBundle\Entity\Bar\Bar
-     */
-    public function getBar()
-    {
-        return $this->bar;
     }
 }
