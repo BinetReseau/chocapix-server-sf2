943ea5a88c0e484ad76361acd2306847db07880f
Nadrieril <nadrieril@gmail.com>
2014-08-05 04:53:25 +0200

    Bugfix: pluralize error handling

--- web/pluralize/pluralize.py
+++ web/pluralize/pluralize.py
@@ -8,18 +8,25 @@ morph_file = 'fr.morph.bin'
 gen_file = 'fr.gen.bin'
 input_word = cgi.FieldStorage().getvalue("word")
 
+print("Content-type: application/json")
+
 p = Popen(['lt-proc', morph_file], stdin=PIPE, stdout=PIPE, universal_newlines=True)
 lex = p.communicate(input = input_word+"\n")[0].strip()
-lex = re.sub("^\\^[^/]+/", "^", lex)
 
-lex = lex.replace("<sg>", "<pl>")
-p = Popen(['lt-proc', '-g', gen_file], stdin=PIPE, stdout=PIPE, universal_newlines=True)
-plural = p.communicate(lex+"\n")[0].strip()
+if re.match("^\\^[^/]+/\\*", lex):
+	print("Status: 404\n")
+	print('{"error":{"code":404,"message":"Not Found"}}')
 
-lex = lex.replace("<pl>", "<sg>")
-p = Popen(['lt-proc', '-g', gen_file], stdin=PIPE, stdout=PIPE, universal_newlines=True)
-singular = p.communicate(lex+"\n")[0].strip()
+else:
+	print()
+	lex = re.sub("^\\^[^/]+/", "^", lex)
 
-print("Content-type: application/json")
-print("")
-print("{\"singular\":\"%s\", \"plural\":\"%s\"}"%(singular, plural))
+	lex = lex.replace("<sg>", "<pl>")
+	p = Popen(['lt-proc', '-g', gen_file], stdin=PIPE, stdout=PIPE, universal_newlines=True)
+	plural = p.communicate(lex+"\n")[0].strip()
+
+	lex = lex.replace("<pl>", "<sg>")
+	p = Popen(['lt-proc', '-g', gen_file], stdin=PIPE, stdout=PIPE, universal_newlines=True)
+	singular = p.communicate(lex+"\n")[0].strip()
+
+	print('{"singular": "%s", "plural": "%s"}'%(singular, plural))
