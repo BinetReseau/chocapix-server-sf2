c60f298eb203c3e8f0643abbc36206ae1b09605d
Nadrieril <nadrieril@gmail.com>
2014-08-11 15:29:58 +0200

    Added MetadataController

---
+++ src/BR/BarBundle/Controller/MetadataController.php
@@ -0,0 +1,28 @@
+<?php
+namespace BR\BarBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Controller\FOSRestController;
+
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+use BR\BarBundle\Entity\Bar\Bar;
+use BR\BarBundle\Entity\Auth\User;
+use BR\BarBundle\Entity\Account\Account;
+
+class MetadataController extends FOSRestController {
+	/**
+	 * @Get("/{bar}/Metadata")
+	 * @ParamConverter("bar", class="BRBarBundle:Bar\Bar", options={"id" = "bar"})
+     */
+	public function getAccountsAction(Bar $bar) {
+		return new Response(file_get_contents("../src/BR/BarBundle/Resources/metadata.json"));
+	}
+}
