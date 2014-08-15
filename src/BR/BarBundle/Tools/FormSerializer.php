ffeeeb27e23834d0fe474fddcf060c5c0a1fbb11
Nadrieril <nadrieril@gmail.com>
2014-08-15 18:25:08 +0200

    FormSerializer now returns number as such instead of as strings

--- src/BR/BarBundle/Tools/FormSerializer.php
+++ src/BR/BarBundle/Tools/FormSerializer.php
@@ -36,8 +36,16 @@ class FormSerializer implements SerializerInterface
         $a = array();
         foreach ($v->children as $key => $child) {
             $o = $this->serializeFormView($child);
-            if(isset($o["block_prefixes"]) and in_array("checkbox", $o["block_prefixes"])) {
-                $o["value"] = $o["checked"];
+            if(isset($o["block_prefixes"])) {
+                if(in_array("checkbox", $o["block_prefixes"])) {
+                    $o["value"] = $o["checked"];
+                }
+                if(in_array("integer", $o["block_prefixes"])) {
+                    $o["value"] = intval($o["value"]);
+                }
+                if(in_array("number", $o["block_prefixes"])) {
+                    $o["value"] = floatval($o["value"]);
+                }
             }
             // unset($o["form"]);
             // $a[$key] = $o;
