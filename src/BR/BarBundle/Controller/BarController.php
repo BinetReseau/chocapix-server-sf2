7a1875a2bfba152b96c3d79a4af74cd215cd8196
Nadrieril <nadrieril@gmail.com>
2014-08-18 19:20:54 +0200

    BarController now with forms

--- src/BR/BarBundle/Controller/BarController.php
+++ src/BR/BarBundle/Controller/BarController.php
@@ -11,13 +11,35 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use BR\BarBundle\Entity\Bar\Bar;
 
 class BarController extends FOSRestController {
+	/**
+	 * @Get("/{bar}/bar")
+	 * @ParamConverter("bar", class="BRBarBundle:Bar\Bar", options={"id" = "bar"})
+	 *
+     * @View()
+     */
+	public function getBarsAction(Bar $bar) {
+		$bars = $this->getDoctrine()->getRepository('BR\BarBundle\Entity\Bar\Bar')
+                ->findAll();
+        return $this->createForm('collection', $bars, array('type' => 'bar'));
+	}
+
+	/**
+	 * @Get("/{bar}/bar/{bar2}")
+	 * @ParamConverter("bar", class="BRBarBundle:Bar\Bar", options={"id" = "bar"})
+	 * @ParamConverter("bar2", class="BRBarBundle:Bar\Bar", options={"id" = "bar2"})
+     * @View()
+     */
+	public function getBarAction(Bar $bar, Bar $bar2) {
+        return $this->createForm('bar', $bar2);
+	}
+
 
 	/**
 	 * @Get("/{bar}")
 	 * @ParamConverter("bar", class="BRBarBundle:Bar\Bar", options={"id" = "bar"})
      * @View()
      */
-	public function getBarAction(Bar $bar) {
-		return $bar;
+	public function getCurrentBarAction(Bar $bar) {
+        return $this->createForm('bar', $bar);
 	}
 }
