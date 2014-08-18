a2226e6b20dba02d3d5c7e206331f62ac2680c19
Nadrieril <nadrieril@gmail.com>
2014-08-18 19:20:48 +0200

    UserController now with forms

--- src/BR/BarBundle/Controller/UserController.php
+++ src/BR/BarBundle/Controller/UserController.php
@@ -1,7 +1,6 @@
 <?php
 namespace BR\BarBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
@@ -10,48 +9,36 @@ use FOS\RestBundle\Controller\Annotations\Get;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 use BR\BarBundle\Entity\Bar\Bar;
+use BR\BarBundle\Entity\Auth\User;
 
 class UserController extends FOSRestController {
-	/**
-	 * @Get("/{bar}/user")
-	 * @ParamConverter("bar", class="BRBarBundle:Bar\Bar", options={"id" = "bar"})
-	 *
+    /**
+     * @Get("/{bar}/user")
+     * @ParamConverter("bar", class="BRBarBundle:Bar\Bar", options={"id" = "bar"})
+     *
      * @View()
      */
-	public function getUsersAction(Bar $bar) {
-		$qb = $this->getDoctrine()->getRepository('BRBarBundle:Account\Account')
-				->createQueryBuilder('a')
-				->select('IDENTITY(a.user)')
-				->where('a.bar = :bar');
+    public function getUsersAction(Bar $bar) {
+        $repository = $this->getDoctrine()
+                ->getRepository('BRBarBundle:Auth\User');
 
-		$qb = $this->getDoctrine()->getRepository('BRBarBundle:Auth\User')
-				->createQueryBuilder('u')
-				->select('u', 'account')
-				->leftjoin('u.accounts', 'account', 'WITH', 'account.bar = :bar')
-				->where('u.id IN ('.$qb->getDQL().')')
-				->orderBy('u.name', 'ASC')
-				->setParameter('bar', $bar);
+        $users = $repository->createQueryBuilder('u')
+                ->select('u')
+                ->leftjoin('u.accounts', 'account', 'WITH', 'account.bar = :bar')
+                ->where('account != 0')
+                ->setParameter('bar', $bar)
+                ->getQuery()->getResult();
+        return $this->createForm('collection', $users, array('type' => 'user'));
+    }
 
-		return $qb->getQuery()->getResult();
-	}
-
-	/**
-	 * @Get("/{bar}/user/{id}")
-	 * @ParamConverter("bar", class="BRBarBundle:Bar\Bar", options={"id" = "bar"})
-	 *
+    /**
+     * @Get("/{bar}/user/{id}")
+     * @ParamConverter("bar", class="BRBarBundle:Bar\Bar", options={"id" = "bar"})
+     * @ParamConverter("user", class="BRBarBundle:Auth\User", options={"id" = "id"})
+     *
      * @View()
      */
-	public function getUserAction(Bar $bar, $id) {
-		$repository = $this->getDoctrine()
-				->getRepository('BRBarBundle:Auth\User');
-
-		$user = $repository->createQueryBuilder('u')
-				->select('u', 'account')
-				->leftjoin('u.accounts', 'account', 'WITH', 'account.bar = :bar')
-		        ->where('u.id = :id')
-				->setParameter('id', $id)
-				->getQuery()->getSingleResult();
-
-		return $user;
-	}
+    public function getUserAction(Bar $bar, User $user) {
+        return $this->createForm('user', $user);
+    }
 }
